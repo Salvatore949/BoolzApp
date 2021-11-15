6d2990c5a6d9a9fbdb2f3d2c8dfcf0b1ce0e147e
Salvatore949 <salvo.mess49@gmail.com>
2021-11-15 16:22:00 +0100

    array

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var whatsupp = new Vue({
+ el:"#contatti",
+ data:{
+     persone:[
+        {
+            image:'img/avatar_1.jpg',
+            text:'Michele',
+        },
+         
+        {
+            image:'img/avatar_2.jpg',
+            text:'Fabio',
+        },
+
+        {
+            image:'img/avatar_3.jpg',
+            text:'Samuele',
+        }, 
+        
+        {
+            image:'img/avatar_4.jpg',
+            text:'Luisa',
+        }  
+      ]  
+    },
+});
