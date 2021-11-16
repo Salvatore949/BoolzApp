3a7cfe58ede3f5626027ab48eed8bb9a5b133df5
Salvatore949 <salvo.mess49@gmail.com>
2021-11-16 17:31:34 +0100

    methods

--- js/script.js
+++ js/script.js
@@ -1,92 +1,107 @@
 //Creo un array di elementi
+// ● Visualizzazione dinamica dei messaggi: t ramite l a direttiva v-for, visualizzare t utti i
+// messaggi relativi al contatto attivo all’interno del pannello della conversazione
+// ● Click sul contatto mostra l a conversazione del contatto cliccato
 var whatsupp = new Vue({
- el:"#container",
- data:{
-    contacts: [
-            {
-                name: 'Michele',
-                avatar: 'img/avatar_1.jpg',
-                visible: true,
-                messages: [
-                    {
-                        date: '10/01/2020 15:30:55',
-                        text: 'Hai portato a spasso il cane?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 15:50:00',
-                        text: 'Ricordati di dargli da mangiare',
-                        status: 'sent'
-                    },
-                    {
-                        date: '10/01/2020 16:15:22',
-                        text: 'Tutto fatto!',
-                        status: 'received'
-                    }
-                ],
-            },
+    el:"#container",
+    data:{
+      contacts: [
+               {
+                   name: 'Michele',
+                   avatar: 'img/avatar_1.jpg',
+                   visible: true,
+                   messages: [
+                       {
+                           date: '10/01/2020 15:30:55',
+                           text: 'Hai portato a spasso il cane?',
+                           status: 'sent'
+                       },
+                       {
+                           date: '10/01/2020 15:50:00',
+                           text: 'Ricordati di dargli da mangiare',
+                           status: 'sent'
+                       },
+                       {
+                           date: '10/01/2020 16:15:22',
+                           text: 'Tutto fatto!',
+                           status: 'received'
+                       }
+                   ],
+               },
+   
+               {
+                   name: 'Fabio',
+                   avatar: 'img/avatar_2.jpg',
+                   visible: false,
+                   messages: [
+                       {
+                           date: '20/03/2020 1 6:30:00',
+                           text: 'Ciao come stai?',
+                           status: 'sent'
+                       },
+                       {
+                           date: '20/03/2020 16:30:55',
+                           text: 'Bene grazie! Stasera ci vediamo?',
+                           status: 'received'
+                       },
+                       {
+                           date: '20/03/2020 16:35:00',
+                           text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                           status: 'sent'
+                       }
+                   ],
+               },
+               {
+                   name: 'Samuele',
+                   avatar: 'img/avatar_3.jpg',
+                   visible: false,                                                                                                                                                              
+                   messages: [
+                           {
+                               date: '28/03/2020 10:10:40',
+                               text: 'La Marianna va in campagna',
+                               status: 'received'
+                           },
+                           {
+                               date: '28/03/2020 10:20:10',
+                               text: 'Sicuro di non aver sbagliato chat?',
+                               status: 'sent'
+                           },
+                           {
+                               date: '28/03/2020 16:15:22',
+                               text: 'Ah scusa!',
+                               status: 'received'
+                           }
+                       ],
+                   },
+               {
+                   name: ' Luisa',
+                   avatar: 'img/avatar_4.jpg',
+                   visible: false,
+                   messages: [
+                       {
+                       date: '10/01/2020 1 5:30:55',
+                       text: 'Lo sai che ha aperto una nuova pizzeria?',
+                       status: 'sent'
+                       },
+                       {
+                       date: '10/01/2020 15:50:00',
+                       text: 'Si, ma preferirei andare al cinema',
+                       status: 'received'
+                       }
+                   ],
+               },
+        ],
+    },
 
-            {
-                name: 'Fabio',
-                avatar: 'img/avatar_2.jpg',
-                visible: true,
-                messages: [
-                    {
-                        date: '20/03/2020 1 6:30:00',
-                        text: 'Ciao come stai?',
-                        status: 'sent'
-                    },
-                    {
-                        date: '20/03/2020 16:30:55',
-                        text: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
-                    },
-                    {
-                        date: '20/03/2020 16:35:00',
-                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
-                    }
-                ],
-            },
-            {
-                name: 'Samuele',
-                avatar: 'img/avatar_3.jpg',
-                visible: true,                                                                                                                                                              
-                messages: [
-                        {
-                            date: '28/03/2020 10:10:40',
-                            text: 'La Marianna va in campagna',
-                            status: 'received'
-                        },
-                        {
-                            date: '28/03/2020 10:20:10',
-                            text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
-                        },
-                        {
-                            date: '28/03/2020 16:15:22',
-                            text: 'Ah scusa!',
-                            status: 'received'
-                        }
-                    ],
-                },
-            {
-                name: ' Luisa',
-                avatar: 'img/avatar_4.jpg',
-                visible: true,
-                messages: [
-                    {
-                    date: '10/01/2020 1 5:30:55',
-                    text: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                    },
-                    {
-                    date: '10/01/2020 15:50:00',
-                    text: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                    }
-                ],
-            },
-        ]
-    }
+    methods: {
+        single_card(utenteSelezionato){
+            for(let i = 0; i < this.contacts.length; i++){
+            this.contacts[i].visible = false}
+            utenteSelezionato.visible = true
+        }
+    
+
+    },
+
+       
 });
